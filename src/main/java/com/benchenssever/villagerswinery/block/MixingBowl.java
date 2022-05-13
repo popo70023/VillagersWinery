60f782f6971853bc7c79defde98dae288ea21efe
LaysDragon <laysdra7265@gmail.com>
2022-05-13 23:51:10 +0800

    攪拌盆的形狀改用static存放

--- src/main/java/com/benchenssever/villagerswinery/block/MixingBowl.java
+++ src/main/java/com/benchenssever/villagerswinery/block/MixingBowl.java
@@ -10,8 +10,8 @@ import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 public class MixingBowl extends Block {
-    private static final VoxelShape INSIDE = makeCuboidShape(2.0D, 4.0D, 2.0D, 14.0D, 16.0D, 14.0D);
-    protected static final VoxelShape SHAPE = VoxelShapes.combineAndSimplify(VoxelShapes.fullCube(), VoxelShapes.or(INSIDE), IBooleanFunction.ONLY_FIRST);
+    private static final VoxelShape INSIDE = makeCuboidShape(2.0D, 3.0D, 2.0D, 14.0D, 16.0D, 14.0D);
+    protected static final VoxelShape SHAPE = VoxelShapes.combineAndSimplify(makeCuboidShape(0,0,0,16,14,16), VoxelShapes.or(INSIDE,makeCuboidShape(2,0,0,14,2,16),makeCuboidShape(0,0,2,16,2,14)), IBooleanFunction.ONLY_FIRST);
 
     public MixingBowl(Properties properties) {
         super(properties);
@@ -19,8 +19,6 @@ public class MixingBowl extends Block {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        VoxelShape INSIDE = makeCuboidShape(2.0D, 3.0D, 2.0D, 14.0D, 16.0D, 14.0D);
-        VoxelShape SHAPE = VoxelShapes.combineAndSimplify(makeCuboidShape(0,0,0,16,14,16), VoxelShapes.or(INSIDE,makeCuboidShape(2,0,0,14,2,16),makeCuboidShape(0,0,2,16,2,14)), IBooleanFunction.ONLY_FIRST);
         return SHAPE;
     }
 }
