7395268c1812218a0cba28e8de076be2cdedfdb6
popo70023 <popo70023@yahoo.com.tw>
2022-05-13 16:19:41 +0800

    方塊破壞掉落與作物收成更新

--- src/main/java/com/benchenssever/villagerswinery/block/GrapeVine.java
+++ src/main/java/com/benchenssever/villagerswinery/block/GrapeVine.java
@@ -1,19 +1,34 @@
 package com.benchenssever.villagerswinery.block;
 
+import com.benchenssever.villagerswinery.registration.RegistryEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.IGrowable;
 import net.minecraft.block.VineBlock;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class GrapeVine extends VineBlock implements IGrowable, ICrop {
@@ -37,6 +52,23 @@ public class GrapeVine extends VineBlock implements IGrowable, ICrop {
     public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
         super.randomTick(state, worldIn, pos, random);
         this.growth(state, worldIn, pos, random);
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        ItemStack stack = player.getHeldItem(handIn);
+        if(stack.getItem() == Items.SHEARS && this.isMaxAge(state)) {
+            if(!worldIn.isRemote()) {
+                worldIn.playSound(null, pos, SoundEvents.ENTITY_SHEEP_SHEAR, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                worldIn.addEntity(new ItemEntity(worldIn, pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, RegistryEvents.grape.get().getDefaultInstance()));
+                worldIn.setBlockState(pos, state.with(this.getAgeProperty(), 0), 2);
+                if (!player.abilities.isCreativeMode) {
+                    stack.attemptDamageItem(1, new Random(), (ServerPlayerEntity) player);
+                }
+            }
+            return ActionResultType.SUCCESS;
+        }
+        return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
     }
 
     @Override
